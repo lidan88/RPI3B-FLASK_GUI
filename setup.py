71cdfa026cf19dc26e3b74fd43cd266f33d69116
DESKTOP-3OV49PP\Embedded <arian89parian@mail.com>
2017-09-14 15:06:59 -0700

    version 1.1 20170914

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tankController',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Embedded',
+    author_email='',
+    description=''
+)
